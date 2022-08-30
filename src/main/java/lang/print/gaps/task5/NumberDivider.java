98bf853ecd2c487825922239c44e53f12d888397
v-nebe <katya.shauneva@gmail.com>
2022-08-30 08:46:38 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        double number = toBeDivided / 5;
-        System.out.println(number);
+        float result = toBeDivided / 5f;
+        System.out.println(result);
     }
 }
