bcec1a7e4c60942e92db7a891cb2d21516fccf3f
v-nebe <katya.shauneva@gmail.com>
2022-08-30 08:29:31 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        toBeDivided /= 5;
+        System.out.println(toBeDivided);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        minutes *= 60;
+        System.out.println(minutes);
     }
 }
